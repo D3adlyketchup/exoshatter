1f6e7701c50e6d238d76461f7d18ce9830beda2f
D3adlyketchup <105149450+D3adlyketchup@users.noreply.github.com>
2026-08-31 18:30:13 +0200

    Rename index.html to index.php and add PHP header
    Converted index.html to index.php and included PHP header.

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require "header.html";
+?>
+
+<main>
+  <h1>Exoshatter</h1>
+  <p class="intro">A few small tools.</p>
+
+  <div class="box">
+    <a href="/qr/">Free QR code maker</a>
+    <a href="/studyflow/">Studyflow</a>
+  </div>
+</main>
+
+<footer>exoshatter.com</footer>
+
+</body>
+</html>
