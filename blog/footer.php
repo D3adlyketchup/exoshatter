a8c50a53e9cf2b54c1d12b1a56e80c6f23feaaaf
D3adlyketchup <105149450+D3adlyketchup@users.noreply.github.com>
2026-09-02 15:40:33 +0200

    Delete blog directory

--- blog/footer.php
+++
@@ -1,8 +0,0 @@
-</main>
-<footer class="site-footer">
-    <div class="footer-inner">
-        <p>&copy; <?= date('Y') ?> Exoshatter. All rights reserved.</p>
-    </div>
-</footer>
-</body>
-</html>
